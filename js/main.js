1c72e2b0047686219a94b4fc949dd7cf060ab9fd
Bread God <Vipcube@users.noreply.github.com>
2022-04-30 20:30:46 +0800

    Finished get json and color level logic.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
 'use strict';
 
+const styles = {
+    greater100: {
+        iconShape: "doughnut",
+        borderWidth: 5,
+        borderColor: "#009a4b"
+    },
+    range40To100: {
+        iconShape: "doughnut",
+        borderWidth: 5,
+        borderColor: "#FFEA00"
+    },
+    range20To40: {
+        iconShape: "doughnut",
+        borderWidth: 5,
+        borderColor: "#F08000"
+    },
+    lower20: {
+        iconShape: "doughnut",
+        borderWidth: 5,
+        borderColor: "#FF0000"
+    },
+    noData: {
+        iconShape: "doughnut",
+        borderWidth: 5,
+        borderColor: "#8A90B4"
+    }
+}
+
 const state = {
     overLayers: {},
     baseLayers: {},
@@ -29,4 +57,60 @@ const initMaps = (mapId) => {
         container: 'sidebar',
         position: 'left',
     }).addTo( state.map ).open('home');
+}
+
+const initGeoJsonPointLayer = () => {
+    state.overLayers['健保藥局庫存'] = L.geoJson( null, {
+        onEachFeature: (feature, layer) => {
+            layer.bindTooltip( (layer) => {
+                return `<b>藥局：</b>${layer.feature.properties.name}<br>
+                    <b>電話：</b>${layer.feature.properties.phone}<br>
+				    <b>地址：</b>${layer.feature.properties.address}<br>
+                    <b>目前庫存：</b>${layer.feature.properties.stock}<br>
+                    <b>資料時間：</b>${layer.feature.properties.updateTime}`;
+            }, {
+                direction:'right',
+                className:"map-tooltip",
+                offset:[0, 0]
+            } );
+
+            (() => {
+                layer.on("click", () => {
+
+                });
+            })(layer, feature.properties);
+        },
+        pointToLayer: (feature, latlng) => {
+            const stock = feature.properties.stock;
+            let stockStyle;
+            if ( stock > 100  ){
+                stockStyle = styles.greater100;
+            } else if ( stock >= 40 && stock <= 100 ){
+                stockStyle = styles.range40To100;
+            } else if ( stock >= 20 && stock < 40 ){
+                stockStyle = styles.range20To40;
+            } else if ( stock < 20 && stock >= 0 ){
+                stockStyle = styles.lower20;
+            } else {
+                stockStyle = styles.noData;
+            }
+
+            return L.marker( latlng, {
+                icon: L.BeautifyIcon.icon( stockStyle )
+            } );
+        }
+    }).addTo( state.map );
+}
+
+const loadBackend = () => {
+    fetch('https://vipcube.github.io/opendata.gov.tw/rapidTestStock.json')
+        .then( response => response.json() )
+        .then( json => {
+            console.log( json );
+            if ( state.overLayers['健保藥局庫存'] ){
+                const layer = state.overLayers['健保藥局庫存'];
+                layer.clearLayers();
+                layer.addData( json );
+            }
+        });
 }
