ecbe5b49aba6034aa9cb4a303ac04912ed499aac
Bread God <Vipcube@users.noreply.github.com>
2022-05-01 23:38:19 +0800

    Finished detail page.

--- js/main.js
+++ js/main.js
@@ -11,7 +11,9 @@ const colors = {
 const state = {
     overLayers: {},
     baseLayers: {},
-    center: [25.0850681831, 121.564750671]
+    center: [25.0850681831, 121.564750671],
+    title: document.getElementById('detailTitle'),
+    content: document.getElementById('detailContent')
 }
 
 /**
@@ -80,7 +82,7 @@ const initGeoJsonPointLayer = () => {
 
             (() => {
                 layer.on("click", () => {
-
+                    updateDetail( feature );
                 });
             })(layer, feature.properties);
         },
@@ -128,4 +130,47 @@ const loadBackend = () => {
 const selectCountry = ( option ) => {
     const array = option.value.split(",").map(Number);
     state.map.flyTo( [array[0], array[1]], array[2] );
+}
+
+const updateDetail = ( feature ) => {
+    state.title.innerHTML = feature.properties.name;
+    state.content.innerHTML = `
+    <table class="table-primary table-dark table-bordered" style="color: white; font-size: 1rem;">
+        <tbody>
+            <tr>
+              <th scope="row" style="width: 100px;">衛福部網址</th>
+              <td><a href="https://www.nhi.gov.tw/QueryN/Query3_Detail.aspx?HospID=${feature.properties.id}" target="_blank">院所明細</a></td>
+            </tr>
+            <tr>
+              <th scope="row">快篩庫存</th>
+              <td>${feature.properties.stock}</td>
+            </tr>
+            <tr>
+              <th scope="row">快篩品牌</th>
+              <td>${feature.properties.label}</td>
+            </tr>
+            <tr>
+              <th scope="row">備註</th>
+              <td>${feature.properties.remark}</td>
+            </tr>
+            <tr>
+              <th scope="row">電話</th>
+              <td>${feature.properties.phone}</td>
+            </tr>
+            <tr>
+              <th scope="row">地址</th>
+              <td>${feature.properties.address}</td>
+            </tr>
+            <tr>
+              <th scope="row">資料更新時間</th>
+              <td>${feature.properties.updateTime}</td>
+            </tr>
+        </tbody>
+    </table>
+    <hr />
+    <div class="btn-group-vertical" role="group" style="width: 100%;">
+        <a href="https://www.google.com/maps/dir/?api=1&destination=${feature.geometry.coordinates[1]},${feature.geometry.coordinates[0]}&travelmode=driving" target="_blank" class="btn btn-info btn-lg btn-block">Google 導航</a>
+    </div>
+    `;
+    state.sidebar.open('detail');
 }
